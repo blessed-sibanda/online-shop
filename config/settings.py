ac9705e31e5d08a8500d0cb7ab70c65b211198ce
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-29 16:01:10 +0200

    implement payment gateway to process credit cards

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#-v7gozrznegovb1mfp#=661zx1ucu_)6a4&c%&$my@nd=d834
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mysite.com']
 
 
 # Application definition
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from .tasks import order_created
@@ -20,8 +20,9 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html',
-                          {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            return redirect('payment:process')
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html',
